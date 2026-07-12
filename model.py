bb1d3540fd6600be87a0eeb1e3786ca8b32e14e5
Nitai7846 <62793438+Nitai7846@users.noreply.github.com>
2026-07-12 15:07:18 -0500

    Step 20: conv2d_grad_bias

--- model.py
+++ model.py
@@ -299,8 +299,10 @@ def conv2d_grad_weights(d_out, cache):
 
     return np.array(d_W_flat)
 
-# Step 20 - conv2d_grad_bias (not yet solved)
-# TODO: implement
+# Step 20 - conv2d_grad_bias
+def conv2d_grad_bias(d_out):
+    # TODO: return a length C_out gradient by reducing d_out over batch and spatial axes
+    return d_out.sum(axis=(0,2,3))
 
 # Step 21 - conv2d_backward (not yet solved)
 # TODO: implement
