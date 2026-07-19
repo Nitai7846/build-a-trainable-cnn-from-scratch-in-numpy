2d31cda06550335b45ed3ca66f15d5b8eef65113
Nitai7846 <62793438+Nitai7846@users.noreply.github.com>
2026-07-19 18:48:43 -0500

    Step 39: adam_bias_correct

--- model.py
+++ model.py
@@ -504,8 +504,10 @@ def adam_update_v(v, grad, beta_two):
     v_t = beta_two*v + (1-beta_two) * (grad)**2 
     return v_t
 
-# Step 39 - adam_bias_correct (not yet solved)
-# TODO: implement
+# Step 39 - adam_bias_correct
+def adam_bias_correct(moment, beta, t):
+    # TODO: return moment divided by (1 - beta**t) to undo Adam's zero-init bias.
+    return moment/(1-beta**t)
 
 # Step 40 - adam_param_step (not yet solved)
 # TODO: implement
